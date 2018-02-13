f14d93eb2abfd594fa0187d4877da3da817ce144
Sandeep0311 <sandeep.raj@tothenew.com>
2018-02-13 16:57:42 +0530

    program created

--- src/Firstprogram.java
+++ src/Firstprogram.java
@@ -2,7 +2,7 @@ public class Firstprogram {
     String s1="sandeep";
     private void replace(){
         System.out.println("old string: "+s1);
-    String s2= s1.replace(s1,"raj");
+    String s2= s1.replace(s1,"naman");
         System.out.println("replaced with: "+s2);
     }
 
--- src/Secondprogram.java
+++ src/Secondprogram.java
@@ -2,18 +2,20 @@ public class Secondprogram {
     String s= " sandeep said sandeep is my name ...only name";
     private void occurance(){
         int count=0;
+        int  i,j;
         String []words=s.split(" ");
         for(String w: words){
-            System.out.println(w);
+            //System.out.println(w);
         }
-        for(int i=0; i<words.length;i++){
-            for(int j=1; j<words.length;j++){
-                if(words[i]==words[j]){
-
-                    System.out.println(words[j] +" "+ count++);
+        for(i=0; i<words.length;i++){
+            for(j=1; j<words.length;j++){
+                if(words[j]==words[i]){
+                    count++;
                 }
-
+                System.out.println(words[j] +" "+ count);
             }
+
+            count=0;
 
         }
 
