87a5b14fbd9a220251dc5ea7b64e4101c2cbb8e2
Sandeep0311 <sandeep.raj@tothenew.com>
2018-02-13 23:24:02 +0530

    program done

--- src/Seventhprogram.java
+++ src/Seventhprogram.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+//
+//Write a program to print your Firstname,LastName & age using static block,static method
+// & static variable respectively
 
 public class Seventhprogram {
+    static{
+        System.out.println("Sandeep");
+    }
+    private static void lastname(){
+        System.out.println("Raj");
+    }
+    static int age=23;
+    public static void main(String[] args) {
+        lastname();
+
+        System.out.println(age);
+    }
 }
