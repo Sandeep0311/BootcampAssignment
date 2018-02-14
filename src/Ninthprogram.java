41744bed5e5faa18ca4c2ae9d875d39edd1e2d3f
Sandeep0311 <sandeep.raj@tothenew.com>
2018-02-14 10:43:57 +0530

    program done

--- src/Ninthprogram.java
+++ src/Ninthprogram.java
@@ -1,7 +1,34 @@
 //Write a program to read text file & print the content of file using String Builder.
 
-public class Ninthprogram {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Ninthprogram {
+    private void readfile(String p){
+        StringBuilder c=new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(p)))
+        {
+
+            String s;
+            while ((s = br.readLine()) != null)
+            {
+                c.append(s).append("\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println(c);
+        }
+
+    }
+
     public static void main(String[] args) {
-        StringBuilder sb=new StringBuilder();
+       Ninthprogram nt=new Ninthprogram();
+       nt.readfile("/home/sandeep/output.txt");
+
     }
 }
