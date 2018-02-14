94fc28664376a3b9584abd04ccf80e9fbaff7a96
Sandeep0311 <sandeep.raj@tothenew.com>
2018-02-14 14:33:16 +0530

    program done

--- src/Tenthprogram.java
+++ src/Tenthprogram.java
@@ -2,47 +2,29 @@
 // house & their prices ).
 
 public class Tenthprogram {
-    enum House {
-        Khan_house, Raj_house, Aashiyana, Gharaana;
-        private void getPrice() {
-            for (House h : House.values()) {
-                System.out.println(h);
+    enum House{
+        flats(25000),duplex(35000),builderflats(20000),Hig(19000),Mig(15000),Lig(10000);
+        private int p;
+        House(int p){
+            this.p=p;
+        }
+        int getPrice(){
+            return p;
+        }
+    }
+    public class EnumExample {
+        EnumExample(){
+            System.out.println("House type Flats has value"+House.flats.values());
+        }
+    }
+        public static void main(String[] args) {
+            System.out.println("Price of houses are ");
+            for(House h:House.values()){
+// new House(25000,35000,20000,19000,15000,10000);
+                System.out.println(
+                        h + " costs " + h.getPrice() + " Rupees.");
             }
         }
-        }
-    Tenthprogram(){
-
-
     }
-    public static void main(String[] args) {
-        Tenthprogram tp=new Tenthprogram();
-     //   tp.getPrice();
-
-    }
-}
 
 
-//####################################################333
-//enum House{
-//    flats(25000),duplex(35000),builderflats(20000),Hig(19000),Mig(15000),Lig(10000);
-//    private int p;
-//    House(int p){
-//        this.p=p;
-//    }
-//    int getPrice(){
-//        return p;
-//    }
-//}
-//public class EnumExample {
-//    EnumExample(){
-//        System.out.println("House type Flats has value"+House.flats.values());
-//    }
-//    public static void main(String[] args) {
-//        System.out.println("Price of houses are ");
-//        for(House h:House.values()){
-//// new House(25000,35000,20000,19000,15000,10000);
-//            System.out.println(
-//                    h + " costs " + h.getPrice() + " Rupees.");
-//        }
-//    }
-//}
